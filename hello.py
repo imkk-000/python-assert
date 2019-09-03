a274e6a46e68c20f92f48c2e3b9498c032be746a
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-09-03 23:51:06 +0700

    Add: main with len of string and power number with exponential operator

---
+++ hello.py
@@ -0,0 +1,13 @@
+def main():
+    msg = "Hello World"
+    print("string: ", msg)
+    print("len of: ", len(msg))
+
+def power():
+    baseNumber = 2
+    powerNumber = 10
+    print(baseNumber ** powerNumber)
+
+if __name__ == "__main__":
+    main()
+    power()
